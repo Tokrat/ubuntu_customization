814442210f3d8e43074f745d5cd51a84dfd19bc6
Lucas <darnis.lucas@gmail.com>
2020-07-17 17:16:50 +0200

    add zsh and start vsc

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ubuntu_customization
+# Ubuntu Customization
+
+Install all my personnal preference for a full Ubuntu 20.04 workstation
+
+```bash
+sudo full_install.sh
+```
+
+## ZSH
+
+Add ZSH with plugins + power10k
+
+```bash
+sudo install_zsh.sh
+```
+
+## Visual Studio Code
+
+```bash
+sudo install_vsc.sh
+```
---
+++ install_zsh.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Install dependancies
+sudo apt-get -y update \
+    && sudo apt-get -y upgrade \
+    && sudo apt-get install -y zsh git curl wget dos2unix autojump
+
+# Install oh my zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install powerlevel10k theme
+# Font
+sudo mkdir -p ~/.fonts/m
+sudo cp -p ./zsh/fonts/* ~/.fonts/m
+sudo fc-cache -f -v
+
+# Theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy all the settings
+sudo cp ./zsh/zshrc ~/.zshrc
+sudo cp ./zsh/p10k.zsh ~/.p10k.zsh
+# dos2unix ~/.zshrc
+# dos2unix ~/.p10k.zsh
+
+sudo chown -R $USER:$USER ~/.oh-my-zsh
